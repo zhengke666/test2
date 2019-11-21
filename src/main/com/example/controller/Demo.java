0b3823e1a8090b9397ae269cdf1ce183ae1f621a
zhengke <15106749867@qq.com>
2019-11-21 13:51:53 +0800

    123456

---
+++ src/main/com/example/controller/Demo.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+/**
+ * @author zhengKe
+ * @date 2019-08-22 10:52
+ * @desc
+ */
+public class Demo {
+
+    protected String a(){
+        return "a";
+    }
+
+    String b (){
+        return "b";
+    }
+
+}
